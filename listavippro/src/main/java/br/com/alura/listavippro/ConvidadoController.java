0d6c17db19f1f9c8e0e5aa336f18e8252aaddc07
Leandro Tavares <letavs@gmail.com>
2018-08-26 16:41:34 -0300

    Add atributo da interface ConvidadoRepository com as caracteristicas de um repository

--- listavippro/src/main/java/br/com/alura/listavippro/ConvidadoController.java
+++ listavippro/src/main/java/br/com/alura/listavippro/ConvidadoController.java
@@ -1,11 +1,17 @@
 package br.com.alura.listavippro;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.listavippro.repository.ConvidadoRepository;
 
 @Controller
 public class ConvidadoController {
 
+	@Autowired
+	private ConvidadoRepository repository;
+	
 	@RequestMapping("/")
 	public String index(){
 		return "index";
