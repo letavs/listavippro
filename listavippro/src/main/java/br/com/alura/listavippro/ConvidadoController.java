1fe1538c2bb28869a8ca9b402f76d24a28644c45
Leandro Tavares <letavs@gmail.com>
2018-08-28 00:22:39 -0300

    Adicionado pagina de cadastro de novo usuario

--- listavippro/src/main/java/br/com/alura/listavippro/ConvidadoController.java
+++ listavippro/src/main/java/br/com/alura/listavippro/ConvidadoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.alura.listavippro.model.Convidado;
 import br.com.alura.listavippro.repository.ConvidadoRepository;
@@ -26,10 +28,23 @@ public class ConvidadoController {
 		model.addAttribute("convidados", convidados);
 		return "listaconvidados";
 	}
-
 	@RequestMapping("convidar")
-	public String convite() {
+	public String convite(){
 		return "convidar";
+	}
+
+	@RequestMapping(value="salvar", method = RequestMethod.POST)
+	public String salvar(@RequestParam("nome")String nome,
+						 @RequestParam("email")String email, 
+						 @RequestParam("telefone")String telefone,
+						 Model model) {
+		Convidado novoConvidado = new Convidado(nome, email, telefone);
+		repository.save(novoConvidado);
+		
+		Iterable<Convidado> convidados = repository.findAll();
+		model.addAttribute("convidados", convidados);
+		
+		return "listaconvidados";
 	}
 
 }
